9e1e97924d67f8b49fcca2f75cd5b7d80f72cf70
RStudent2019 <tadair@fin101.org>
2019-08-01 09:01:11 -0400

    Re-added my name
    Added RStudent2019 to R script

--- Intro to R.R
+++ Intro to R.R
@@ -1,6 +1,7 @@
 # R script to accopany Intro to R
 # Written by Troy Adair
 # Copy for RStudent 2019
+# Customized by RStudent2019
 #######################################################
 
 # Assignment Examples
